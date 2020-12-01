3713eb4374dd0ac0a06a488539fad3f144eb1cec
AndyEvan <58259235+HenryEvan@users.noreply.github.com>
2020-12-01 16:18:34 -0300

    Update login.sh

--- Interface/Arquivos/login.sh
+++ Interface/Arquivos/login.sh
@@ -2,7 +2,7 @@
 
 # Variaveis:
 
-tam_log=$(wc -l ./bin/logins.txt | cut -d ' ' -f1)
+tam_log=$(wc -l ./Arquivos/logins.txt | cut -d ' ' -f1)
 
 # Contadores:
 
@@ -16,15 +16,15 @@ while [ $aut_user != 1 ] ; do
 	read -p "User: " ent_user
 
 	for ((i=1; i<=$tam_log; i++)) do
-		s1=$(cat ./bin/logins.txt | cut -d ' ' -f2)
+		s1=$(cat ./Arquivos/logins.txt | cut -d ' ' -f2)
 		loop_user=$(echo $s1 | cut -d ' ' -f$i)
 		if [ "$loop_user" = "$ent_user" ]; then
 			aut_user=1
-			./bin/space.sh
+			./Arquivos/space.sh
 		fi
 	done
 	if [ $aut_user != 1 ]; then
-		./bin/space.sh
+		./Arquivos/space.sh
 		echo -e "Usuario nao encontrado, tente novamente ou use CTRL+C pra sair.\n"
 	fi
 done
@@ -35,7 +35,7 @@ while [ $aut_senha != 1 ] ; do
 	echo -e "User: $ent_user"
 	read -p "Senha: " ent_senha
 	for ((i=1; i<=$tam_log; i++)) do
-		s2=$(cat ./bin/logins.txt | cut -d ' ' -f3)
+		s2=$(cat ./Arquivos/logins.txt | cut -d ' ' -f3)
 		loop_senha=$(echo $s2 | cut -d ' ' -f$i)
 		if [ "$loop_senha" = "$ent_senha" ]; then
 			aut_senha=1
@@ -43,14 +43,14 @@ while [ $aut_senha != 1 ] ; do
 		fi
 	done
 	if [ $aut_senha != 1 ]; then
-		./bin/space.sh
+		./Arquivos/space.sh
 		echo -e "Senha incorreta, tente novamente ou use CTRL+C pra sair.\n"
 	fi
 done
 
 while [ $c_login = 1 ]; do
 
-	./bin/space.sh
+	./Arquivos/space.sh
 	echo -e "Logado no Servidor de Monitoramento - $(date +"%T")\n"
 	echo -e "Usuario: $ent_user - IP Servidor: ?\n"
 #	echo -e "Opcoes:\n\n1 - Monitorar um Servidor \n2 - Configurar um novo Servidor\n3 - Alterar Configuracoes\n4 - Exibir Graficos\n5 - Sair\n"
