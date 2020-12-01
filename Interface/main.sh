b216adf3d11fb59ac25bcec54d0448880d0aa22c
AndyEvan <58259235+HenryEvan@users.noreply.github.com>
2020-12-01 16:16:57 -0300

    Update main.sh

--- Interface/main.sh
+++ Interface/main.sh
@@ -6,19 +6,19 @@ Contador_Menu=0
 
 # Menu:
 
-./bin/space.sh
+./Arquivos/space.sh
 echo -e "Interface de Acesso ao Monitoramento - $(date +"%T")"
-./bin/login.sh
+./Arquivos/login.sh
 
 while [ $Contador_Menu != 1 ] ; do
-	./bin/space.sh
+	./Arquivos/space.sh
 	echo -e "Interface de Acesso ao Monitoramento - $(date +"%T")"
 	echo -e "\n1 - Logar no Servidor\n5 - Sair\n"
 	read -p "> " Entrada_Menu
 	case $Entrada_Menu in
 		1)
-			./bin/space.sh
-			./bin/login.sh;;
+			./Arquivos/space.sh
+			./Arquivos/login.sh;;
 		5)
 			Contador_Menu=1
 			clear;;
