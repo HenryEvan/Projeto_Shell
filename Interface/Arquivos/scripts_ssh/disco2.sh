e116b03dd55371cf9c14b1e248b4a0a50f35e301
felipe cardoso dias <felipe@debian.felipe>
2020-12-08 11:25:52 -0300

    adicionando novos diretórios

--- Interface/Arquivos/scripts_ssh/disco2.sh
+++ Interface/Arquivos/scripts_ssh/disco2.sh
@@ -4,4 +4,4 @@ rm -Rf disco_log.txt
 
 df -h >> disco_log.txt
 
-scp disco_log.txt felipe@192.168.1.5:~/Projeto_Shell/Interface/Arquivos/log
+scp disco_log.txt felipe@192.168.1.5:~/Projeto_Shell/Interface/Arquivos/log/latencia_dir
